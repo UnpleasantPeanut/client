7a25197f346e6ba16dcd63d0957a81d2170df889
Kevin Faulhaber <kevin.faulhaber@gmail.com>
2022-06-26 11:30:43 +0200

    format

--- README.md
+++ README.md
@@ -2,8 +2,12 @@
 
 ## build
 
-deno bundle lib/mod.ts public/javascript/build/upc-bundle.js
+```bash
+SERVER_URL=ws://127.0.0.1:3012 deno bundle lib/mod.ts public/javascript/build/upc-bundle.js
+```
 
 ## server
 
+```bash
 deno run --allow-env --allow-read --allow-net server/main.ts
+```
--- lib/mod.ts
+++ lib/mod.ts
@@ -1,10 +1,19 @@
 import getContext from "./canvas.ts";
 import game from "./game.ts";
+import loadSocketConnection from "./socket.ts";
 
 const ctx = getContext("canvas");
 
 let counter = 0;
 const colors = ["red", "blue", "green"];
+
+const SERVER_URL = Deno.env.get("SERVER_URL");
+
+if (!SERVER_URL) {
+  throw new Error("Server url is not set");
+}
+
+loadSocketConnection(SERVER_URL);
 
 const update = () => {
   counter += 1;
