7f6f5bff06576bd9b5f9b61a4ae4378b975ca076
Kevin Faulhaber <kevin.faulhaber@gmail.com>
2022-06-26 12:34:45 +0200

    chore: format

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,28 +1,33 @@
 interface EnvBuildParams {
-    SERVER_URL: string;
+  SERVER_URL: string;
 }
 
-const OUTPUT = Deno.cwd() + '/public/javascript/build/upc-bundle.js';
+const OUTPUT = Deno.cwd() + "/public/javascript/build/upc-bundle.js";
 
 const build = async (envBuildParams: EnvBuildParams) => {
-    const process = Deno.run({ cmd: [ 
-        'deno', 
-        'bundle',
-        Deno.cwd() + '/lib/mod.ts',
-        OUTPUT
-    ]});
-    await process.status();
+  const process = Deno.run({
+    cmd: [
+      "deno",
+      "bundle",
+      Deno.cwd() + "/lib/mod.ts",
+      OUTPUT,
+    ],
+  });
+  await process.status();
 
-    const bundle = await Deno.readTextFile(OUTPUT);
+  const bundle = await Deno.readTextFile(OUTPUT);
 
-    const parsedBundle = Object.entries(envBuildParams).reduce((acc, [key, value]) => {
-        const searchString = `Deno\\.env\\.get\\("${key}"\\)`;
-        const regExp = new RegExp(searchString, 'g')
-        acc = acc.replace(regExp, `"${value}"`);
-        return acc;
-    }, bundle);
+  const parsedBundle = Object.entries(envBuildParams).reduce(
+    (acc, [key, value]) => {
+      const searchString = `Deno\\.env\\.get\\("${key}"\\)`;
+      const regExp = new RegExp(searchString, "g");
+      acc = acc.replace(regExp, `"${value}"`);
+      return acc;
+    },
+    bundle,
+  );
 
-    await Deno.writeTextFile(OUTPUT, parsedBundle);
-}
+  await Deno.writeTextFile(OUTPUT, parsedBundle);
+};
 
 export default build;
--- server/main.ts
+++ server/main.ts
@@ -2,7 +2,7 @@ import { Application } from "https://deno.land/x/oak@v10.6.0/mod.ts";
 import build from "../scripts/build.ts";
 
 await build({
-    SERVER_URL: Deno.env.get('SERVER_URL') ?? 'ws://127.0.0.1:3012'
+  SERVER_URL: Deno.env.get("SERVER_URL") ?? "ws://127.0.0.1:3012",
 });
 
 const app = new Application();
